f4954fc97a27540a47e516d6f603ca8a938266c7
nattyphysicist <natpersaud@gmail.com>
2017-03-25 15:42:09 -0400

    Update icecast.sh
    just start it

--- icecast.sh
+++ icecast.sh
@@ -1,18 +1,2 @@
 #!/bin/sh
-
-start() {
-  exec icecast2 -b -c /etc/icecast2/icecast.xml
-}
-
-stop() {
-  exec pkill icecast2
-}
-
-case $1 in
-  start)
-    start
-    ;;
-  stop)
-    stop
-    ;;
-esac
+exec icecast2 -b -c /etc/icecast2/icecast.xml
